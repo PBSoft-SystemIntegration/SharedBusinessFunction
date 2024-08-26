51a948da4824bed7c4c35d7b4e2bd336ccffc660
EmilS <emilstephansen@hotmail.com>
2024-08-26 10:46:52 +0200

    Add project files.

---
+++ AdditionServer/AdditionServer.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+TcpListener server = new TcpListener(IPAddress.Any, 8080);
+server.Start();
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    NetworkStream stream = client.GetStream();
+    byte[] buffer = new byte[1024];
+    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    var message = JsonConvert.DeserializeObject<dynamic>(receivedData);
+    Console.WriteLine($" godt : Number1={message.Number1}, " +
+      $"Number2={message.Number2} from client");
+
+    var responseData = new {Result = message.Number1 + message.Number2 };
+    string responseString = JsonConvert.SerializeObject(responseData); 
+    byte[] resposeBytes = Encoding.UTF8.GetBytes(responseString);
+
+    stream.Write(resposeBytes, 0, resposeBytes.Length);
+    stream.Close();
+    client.Close();
+}
---
+++ SharedBusinessFunction/AdditionClient.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Text;
+
+string serverIp = "127.0.0.1";
+TcpClient client = new TcpClient(serverIp, 8080);
+NetworkStream stream = client.GetStream();
+var message = new { Number1= 2, Number2 = 4 };
+string jsonData = JsonConvert.SerializeObject(message);
+byte[] data = Encoding.UTF8.GetBytes(jsonData);
+stream.Write(data, 0, data.Length);
+
+// Modtag svar fra serveren
+byte[] buffer = new byte[1024];
+int bytesRead = stream.Read(buffer, 0, buffer.Length);
+string responseData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine("Server response: " + responseData);
+stream.Close();
+client.Close();
