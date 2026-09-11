5e2454c11e450fcde7b95aba3ddbe1e3a2c39da8
emil <emst@eadania.dk>
2026-09-11 09:25:51 +0200

    updated to net10

--- AdditionServer/AdditionServer.cs
+++ AdditionServer/AdditionServer.cs
@@ -13,7 +13,7 @@ while (true)
     int bytesRead = stream.Read(buffer, 0, buffer.Length);
     string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
     var message = JsonConvert.DeserializeObject<dynamic>(receivedData);
-    Console.WriteLine($" godt : Number1={message.Number1}, " +
+    Console.WriteLine($" got : Number1={message.Number1}, " +
       $"Number2={message.Number2} from client");
 
     var responseData = new {Result = message.Number1 + message.Number2 };
